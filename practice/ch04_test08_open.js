9fdd1706bbe925968a84fdcf4101c615bbb6b124
junblee <topguide82@gmail.com>
2019-10-04 23:55:09 +0900

    openFlag, Buffer.alloc()

--- practice/ch04_test08_open.js
+++ practice/ch04_test08_open.js
@@ -1,14 +1,16 @@
 var fs = require('fs');
 
-fs.open('./output.txt','w',function(err,fd){
+// 파일에 데이터를 씁니다
+fs.open('./newfile1004','w',function(err,fd){
     if(err) throw err;
 
-    var buf = new Buffer('안녕!\n');
+    var buf = new Buffer('Hello\n');
     fs.write(fd, buf, 0, buf.length, null, function(err, written, buffer) {
         if(err) throw err;
-        console.log(err, written, buffer);
-        fs.close(fd, function(){
-            console.log('file open write close');
+
+            console.log(err, written, buffer);
+        fs.close(fd,function() {
+            console.log('file open complete');
         });
     });
 });
