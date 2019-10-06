3dbc1487ff37d17980bd1b52ae7b25f346fe354c
junblee <topguide82@gmail.com>
2019-10-06 23:53:00 +0900

    on()

--- practice/ch05_test1_server.js
+++ practice/ch05_test1_server.js
@@ -1,8 +1,10 @@
+// 웹 서버는 클라이언트에서 HTTP 프로토콜로 요청한 정보를 처리한 후 응답을 보내 주는 역할
+
 var http = require('http'); // 웹 서버 객체 생성시 필요한 http모듈
 
-var server = http.createServer(); // 웹 서버 객체 생성. createServer()메서드 호출시 서버 객체 반환
+var server = http.createServer(); // 웹 서버 객체 생성. createServer()메서드 호출시 서버 객체 반환하므로 이를 통해 웹 서버 객체 상성
 var port = 3000; // 웹 서버를 시작하여 3000번 포트에서 대기
 
-server.listen(port, function() {
+server.listen(port, function() { // listen()메소드를 호출하여 특정 포트에서 대기하도록 설정
     console.log('Starting ebServer, : %d', port);
-});
+}); 
--- practice/ch05_test2_otherEvent.js
+++ practice/ch05_test2_otherEvent.js
@@ -2,20 +2,20 @@ var http = require('http');
 var server = http.createServer();
 var port = 3000;
 
-server.listen(port, function(){
+server.listen(port, function(){ // 3000번 포트에서 대기
     console.log("WebServer Starting : %d", port);
 });
 
 // client connection Event processing
 //// Q-1. socket??
-server.on('connection', function(socket){ // socket객체(클라이언트연결정보를 가짐)가 파라미터로 전달
-    var addr = socket.address(); // 클라이언트의 IP와 포트정보확인가능
+server.on('connection', function(socket){ // 웹 서버에 연결되면 connection이벤트 발생. on()메서드 호출시 첫 바라미터로 connection 이벤트 전달. 연결된 후 콜백함수 호출시 socket객체(클라이언트연결정보를 가짐)가 파라미터로 전달
+    var addr = socket.address(); // address()메소드를 호출하여 클라이언트의 IP와 포트정보확인
     console.log('Access the client : %s %d', addr.address, addr.port);
 });
 
 // Event Processing client request
 // ch05_03 request event's callback function
-server.on('request',function(req,res){ // on메서드는 이벤트처리의 기본 메소드, res객체이용 콜백함수는 클라이언트에게 응답
+server.on('request',function(req,res){ // cli가 특정패스로 요청시 request이벤트 발생. on메서드로 이벤트처리. res객체이용 콜백함수는 클라이언트에게 응답
     console.log('enter the client"s Request');
     
     // res객체의 writeHead(응답보낼헤더생성함),write(응답본문데이터생성,다수호출가능),end(cli로 응답전송, 파라미터에 데이터 있으면 이 데이터 포함하여 응답 전송)메소드를 사용하면 클라이언트에게 응답전달가능
